fcbfd719500813310f21013657db0dc037927953
José Jaime Ramírez Calvo <mr.ljaime@gmail.com>
2019-09-08 22:17:27 -0500

    Para obtener elementos de pago, guardarlos y otras cosillas

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -14,11 +14,46 @@ export class OrdersService {
   }
   
   /**
+   * Add order
+   */
+  public createOrder(formData: any) {
+    return this.apiGateway.doPost('/orders/', formData);
+  }
+  
+  /**
    * Get all orders
    *
    * @returns Observable<ResponseInterface>
    */
   public getOrders() {
     return this.apiGateway.doGet('/orders/', {});
+  }
+  
+  /**
+   * Get order by id
+   *
+   * @param id
+   */
+  public getOrder(id: number) {
+    return this.apiGateway.doGet('/orders/' + id, {});
+  }
+  
+  /**
+   * Get elements by order
+   *
+   * @param id
+   */
+  public getProducts(id: number) {
+    return this.apiGateway.doGet('/orders/' + id + '/products', {});
+  }
+  
+  /**
+   * Add element to order
+   *
+   * @param order
+   * @param formData
+   */
+  public addElement(order: number, formData: any) {
+    return this.apiGateway.doPost('/orders/' + order + '/addElements', formData);
   }
 }
