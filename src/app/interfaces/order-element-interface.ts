a070d96cff978771bc7e86b46311d601e6c1094f
José Jaime Ramírez Calvo <mr.ljaime@gmail.com>
2019-09-15 18:03:37 -0500

    Variables para comisiones

--- src/app/interfaces/order-element-interface.ts
+++ src/app/interfaces/order-element-interface.ts
@@ -4,4 +4,5 @@ export interface OrderElementInterface {
   qty: number;
   price: number;
   amount: number;
+  interest: number;
 }
--- src/app/interfaces/order-interface.ts
+++ src/app/interfaces/order-interface.ts
@@ -1,8 +1,10 @@
 export interface OrderInterface {
   id: number;
   total: number;
+  interest: number;
   paid: boolean;
   sent: boolean;
+  closed: boolean;
   createdAt: string;
   elements: number;
 }
